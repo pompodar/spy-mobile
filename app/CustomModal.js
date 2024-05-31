1778afdae9ad34cc25d8fed79be04537fca7f339
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-05-31 15:41:08 +0300

    add modal and admin leaving functionality

---
+++ app/CustomModal.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const CustomModal = ({ visible, onClose, onConfirm, title, message }) => {
+  return (
+    <Modal
+      transparent={true}
+      visible={visible}
+      animationType="slide"
+    >
+      <View style={styles.modalOverlay}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>{title}</Text>
+          <Text style={styles.modalMessage}>{message}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={onClose} style={styles.button}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onConfirm} style={[styles.button, styles.confirmButton]}>
+              <Text style={styles.buttonText}>Confirm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  modalContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalMessage: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#ddd',
+    marginHorizontal: 10,
+  },
+  confirmButton: {
+    backgroundColor: '#f00',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
+
+export default CustomModal;
--- app/Game.js
+++ app/Game.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, TextInput, Button, Image, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { auth as firebaseAuth, db } from './config/firebase';
@@ -8,21 +8,18 @@ import axios from 'axios';
 import { router } from 'expo-router';
 import { useGlobalSearchParams } from "expo-router";
 import { LinearGradient } from 'expo-linear-gradient';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 import {
   collection,
-  query, 
-  orderBy, 
+  query,
   onSnapshot,
-  getDocs,
   getDoc,
-  addDoc,
   updateDoc,
   deleteDoc,
   doc,
-  setDoc
 } from "firebase/firestore";
+import CustomModal from './CustomModal.js'; // Import your modal component
 
 const Stack = createStackNavigator();
 
@@ -31,6 +28,7 @@ function GameScreen({ navigation }) {
   const [players, setPlayers] = useState([]);
   const [admin, setAdmin] = useState([]);
   const [user, setUser] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const { gameId, gameCode } = useGlobalSearchParams();
 
@@ -39,275 +37,198 @@ function GameScreen({ navigation }) {
       setUser(currentUser || '');
       // Fetch players for the current game
       const fetchData = async () => {
-          try {
-              const response = await axios.get(`https://spy.blobsandtrees.online/api/game/${gameId}/${currentUser?.email}/players`);
-
-                if (!response.data.players.some(item => item.email === currentUser?.email)) {
-                    alert(`User left the game. Redirecting to home screen...`);
-
-                    router.replace("/");
-
-                    return;
-
-                }
-
-                console.log(response);
-                setPlayers(response.data.players);
-                setRound(response.data.round);
-
-          } catch (error) {
-              console.error('Error fetching players:', error);
-              router.replace("/");
-
+        try {
+          const response = await axios.get(`https://spy.blobsandtrees.online/api/game/${gameId}/${currentUser?.email}/players`);
+          if (!response.data.players.some(item => item.email === currentUser?.email)) {
+            console.log(`User left the game. Redirecting to home screen...`);
+            router.replace("/");
+            return;
           }
+          setPlayers(response.data.players);
+          setRound(response.data.round);
+        } catch (error) {
+          console.error('Error fetching players:', error);
+          alert("hmm");
+          router.replace("/");
+        }
       };
 
       const fetchAdmin = async () => {
         try {
-            const response = await axios.get(`https://spy.blobsandtrees.online/api/game/${currentUser?.email}/admin`);
-  
-            setAdmin(response.data);
-
-            console.log(admin);
+          const response = await axios.get(`https://spy.blobsandtrees.online/api/game/${currentUser?.email}/admin`);
+          setAdmin(response.data);
         } catch (error) {
-            console.error('Error fetching players:', error);
+          console.error('Error fetching players:', error);
         }
       };
-  
+
       fetchAdmin();
 
-      const q = query(collection(db, 'gameRooms'))
+      const q = query(collection(db, 'gameRooms'));
       onSnapshot(q, (querySnapshot) => {
         fetchData();
-      })
+      });
 
       fetchData();
-  
     });
     return () => unsubscribe();
   }, []);
 
   const startNewRound = async () => {
-        userEmail = user?.email || "";
+    const userEmail = user?.email || "";
+    try {
+      const response = await axios.post(`https://spy.blobsandtrees.online/api/games/${gameId}/${userEmail}/${round + 1}/rounds`);
+      setPlayers(response.data.players);
+      setRound(round + 1);
 
-        try {
-            const response = await axios.post(`https://spy.blobsandtrees.online/api/games/${gameId}/${userEmail}/${round + 1}/rounds`);
-            setPlayers(response.data.players);
+      try {
+        const gameDocRef = doc(db, "gameRooms", gameId);
+        await updateDoc(gameDocRef, {
+          round: round + 1,
+        });
+      } catch (err) {
+        console.error('Error setting round for game:', err);
+      }
+    } catch (error) {
+      console.error('Error starting new round:', error);
+    }
+  };
 
-            console.log(response);
-            setRound(round + 1);
+  const leaveGame = async () => {
+    const userEmail = user?.email || "";
+    try {
+      await axios.delete(`https://spy.blobsandtrees.online/api/game/${gameId}/${userEmail}/leave`);
 
-            try {
-            // Get the reference to the game document
-            const gameDocRef = doc(db, "gameRooms", gameId);
-            
-            // Update the game document with the updated players array
+      try {
+        const gameDocRef = doc(db, "gameRooms", gameId);
+        const gameDocSnap = await getDoc(gameDocRef);
+        if (gameDocSnap.exists()) {
+          const currentPlayers = gameDocSnap.data().players || [];
+          const playerIndex = currentPlayers.indexOf(userEmail);
+          if (playerIndex !== -1) {
+            const updatedPlayers = [...currentPlayers.slice(0, playerIndex), ...currentPlayers.slice(playerIndex + 1)];
             await updateDoc(gameDocRef, {
-                round: round + 1,
+              players: updatedPlayers,
             });
-
-            } catch (err) {
-            console.error('Error setting round for game:', err);
+            if (updatedPlayers.length === 0) {
+              await deleteDoc(gameDocRef);
             }
-
-            setRound(round + 1);
-        } catch (error) {
-            console.error('Error starting new round:', error);
+          }
         }
-    };
+      } catch (err) {
+        console.error('Error removing player from game:', err);
+      }
 
-    const leaveGame = async () => {
-        userEmail = user?.email || "";
+      router.replace("/");
+    } catch (error) {
+      console.error('Error leaving game:', error);
+    }
+  };
 
-        try {
-              await axios.delete(`https://spy.blobsandtrees.online/api/game/${gameId}/${userEmail}/leave`);
-    
-              try {
-                // Get the reference to the game document
-                const gameDocRef = doc(db, "gameRooms", gameId);
-                
-                // Get the current data of the game document
-                const gameDocSnap = await getDoc(gameDocRef);
-                if (gameDocSnap.exists()) {
-                  // Extract the current players array
-                  const currentPlayers = gameDocSnap.data().players || [];
-              
-                  // Check if the player to remove exists in the players array
-                  const playerIndex = currentPlayers.indexOf(userEmail);
-                  if (playerIndex !== -1) {
-                    // Remove the player from the players array
-                    const updatedPlayers = [...currentPlayers.slice(0, playerIndex), ...currentPlayers.slice(playerIndex + 1)];
-              
-                    // Update the game document with the updated players array
-                    await updateDoc(gameDocRef, {
-                      players: updatedPlayers,
-                    });
-              
-                    console.log('Player removed from the game successfully');
-              
-                    // Check if there are no more players in the game
-                    if (updatedPlayers.length === 0) {
-                      // Delete the game document
-                      await deleteDoc(gameDocRef);
-                      console.log('Game document deleted as there are no more players');
-                    }
-                  } else {
-                    console.log('Player does not exist in the game');
-                  }
-                } else {
-                  console.error('Game document does not exist');
-                }
-              } catch (err) {
-                console.error('Error removing player from game:', err);
-              }
-              
-    
-              router.replace("/");
-    
-            } catch (error) {
-              console.error('Error leaving game:', error);
-          }
-      };    
-  
+  const confirmLeaveGame = () => {
+    setIsModalVisible(true);
+  };
+
   return (
-      <LinearGradient
-        colors={['rgba(68,58,85,1)', 'rgba(136,51,81,1)']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-        style={styles.container}
+    <LinearGradient
+      colors={['rgba(68,58,85,1)', 'rgba(136,51,81,1)']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.container}
+    >
+      <Text style={styles.gameText}>Game Code: {gameCode}</Text>
+      <Image style={styles.logo} source={require('../assets/android-chrome-512x512.png')} />
+      <Text style={styles.gameTextRound}>Round: {round}</Text>
+
+      {players.length < 3 && (
+        <Text style={styles.gameTextWaiting}>Waiting for players</Text>
+      )}
+
+      <div style={{display: 'flex', gap: 8, flexDirection: 'row', justifyContent:'space-around', marginBottom: 10}}>
+
+        {admin.isAdmin && players.length > 2 && (
+          <TouchableOpacity onPress={() => startNewRound()}
+              style={{ backgroundColor: '#a7e9e6', padding: 8, paddingHorizontal: 20, borderRadius: 10, marginBottom: 14 }}
+          >
+            <Entypo name="chevron-with-circle-right" size={20} color="white" />
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity
+          style={{ backgroundColor: 'rgb(239 68 68)', padding: 8, paddingHorizontal: 20, borderRadius: 10, marginBottom: 14 }}
+          onPress={confirmLeaveGame}
         >
-        <Text style={styles.gameText}>Game Code: {gameCode}</Text>
+          <AntDesign name="poweroff" size={16} color="white" />
+        </TouchableOpacity>
+      
+      </div>
 
-        <Image style={styles.logo} source={require('../assets/android-chrome-512x512.png')} />
+      {players.length > 0 && players.map(player => (
+        <View key={player.id} style={styles.playerContainer}>
+          <Text style={styles.gameText}>{player.name} {player.role === 'administrator' && <Text style={{ color: "rgb(167 233 230)" }}>(Admin)</Text>}</Text>
+          <Text style={styles.gameText}>{player.location}</Text>
+        </View>
+      ))}
 
-        <Text style={styles.gameTextRound}>Round: {round}</Text>
-
-        {(admin.isAdmin && players.length > 2) &&
-            <TouchableOpacity
-                onPress={() => startNewRound()}
-            >
-              <Text style={styles.gameText}>
-                new round
-              </Text>
-            </TouchableOpacity>
-          }
-
-            <TouchableOpacity
-                style={{backgroundColor: 'rgb(239 68 68)', padding: 10, paddingHorizontal: 20, borderRadius: 10}}
-                onPress={() => leaveGame()}
-            >
-              
-                  <AntDesign name="poweroff" size={16} color="white" />
-
-            </TouchableOpacity>
-
-        {players.length > 0 && players.map(player => (
-                <div key={player.id} className="">
-                <h2 style={styles.gameText}>{player.name} {player.role === 'administrator' && <span style={{color: "rgb(167 233 230) "}}>(Admin)</span>}</h2>
-                <p style={styles.gameText}>{player.location}</p>
-                </div>
-            ))}
+      <CustomModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        onConfirm={() => {
+          setIsModalVisible(false);
+          leaveGame();
+        }}
+        title="Confirm Leave Game"
+        message="Are you sure you want to leave the game? As the admin, leaving will end the game for everyone."
+      />
     </LinearGradient>
   );
 }
 
-
 export default function App() {
   return (
-      <NavigationContainer independent={true}>
-        <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen
-                name="Game" component={GameScreen}
-                options={{
-                  headerShown: false
-                }}
-            />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Game" component={GameScreen}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    // justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
     paddingTop: 60,
-    backgroundColor: 'linear-gradient(90deg, rgba(68,58,85,1) 0%, rgba(136,51,81,1) 100%)',
-  },
-  authenticatedContainer: {
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  guestContainer: {
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#FFD700',
-    marginBottom: 16,
   },
   logo: {
     width: 160,
     height: 160,
-    borderRadius: '50%',
+    borderRadius: 80,
     marginBottom: 16,
-  },
-  button: {
-    backgroundColor: '#6A0DAD',
-    padding: 12,
-    borderRadius: 8,
-    marginTop: 8,
-    marginBottom: 8,
-  },
-  buttonText: {
-    color: '#FFD700',
-    fontWeight: 'bold',
-  },
-  inputContainer: {
-    width: '100%',
-    alignItems: 'center',
-  },
-  label: {
-    color: '#FFD700',
-    marginBottom: 8,
-  },
-  input: {
-    width: '100%',
-    padding: 8,
-    borderColor: '#FFD700',
-    borderWidth: 1,
-    borderRadius: 8,
-    color: '#FFD700',
-    marginBottom: 16,
-  },
-  errorText: {
-    color: 'red',
-    marginTop: 8,
-  },
-  text: {
-    color: '#FFD700',
-    marginBottom: 8,
-  },
-  gameContainer: {
-    flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-    backgroundColor: 'transparent',
   },
   gameText: {
     color: '#FFD700',
     fontSize: 18,
     marginBottom: 16,
   },
-
   gameTextRound: {
     color: '#FFD700',
     fontSize: 14,
     marginBottom: 16,
+  },
+  gameTextWaiting: {
+    color: '#FFD700',
+    fontSize: 16,
+    marginBottom: 16,
+    fontStyle: 'italic',
+  },
+  playerContainer: {
+    alignItems: 'center',
   },
 });
